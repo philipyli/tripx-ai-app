36fd3974a12e9e87d6a6635db394c38c73042495
Phil Li <philipyli@yahoo.com>
2016-11-16 23:41:49 -0800

    Initial commit

---
+++ apiAISampleApp/src/main/java/ai/api/sample/Config.java
@@ -0,0 +1,30 @@
+package ai.api.sample;
+
+/***********************************************************************************************************************
+ *
+ * API.AI Android SDK -  API.AI libraries usage example
+ * =================================================
+ *
+ * Copyright (C) 2014 by Speaktoit, Inc. (https://www.speaktoit.com)
+ * https://www.api.ai
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
+
+public abstract class Config {
+    // copy this keys from your developer dashboard
+    public static final String ACCESS_TOKEN = "77025ffbde034a7c886a05c9b72e4ef7";
+    public static final String SUBSCRIPTION_KEY = "0fb58c57-99b1-4cd6-90dd-8b94b90e16a9";
+//    public static final String ACCESS_TOKEN = "3485a96fb27744db83e78b8c4bc9e7b7";
+//    public static final String SUBSCRIPTION_KEY = "cb9693af-85ce-4fbf-844a-5563722fc27f";
+}
